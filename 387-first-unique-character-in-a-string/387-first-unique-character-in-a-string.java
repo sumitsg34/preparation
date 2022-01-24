940633e3c407afc5cea20827d899ba29d1c1fc76
Sumeet Gajbhar <sumit.sg34@gmail.com>
2022-01-24 10:55:46 +0530

    Time: 23 ms (48.07%), Space: 39.9 MB (53.31%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -4,12 +4,12 @@ class Solution {
         Map<Character, Integer> visited = new LinkedHashMap<>();
         
         for(int i=0; i < s.length() ; i++) {
-            visited.put(new Character(s.charAt(i)), visited.getOrDefault(new Character(s.charAt(i)), 0) +1);
+            visited.put(s.charAt(i), visited.getOrDefault(s.charAt(i), 0) +1);
         }
         
         for(int i=0; i < s.length() ; i++) {
         
-            if(visited.get(new Character(s.charAt(i))) == 1)
+            if(visited.get(s.charAt(i)) == 1)
                 return i;
         }
         
